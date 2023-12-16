6ecbb0919a3e2e16d4e61a9ddb88ae5f654bd153
DimaOsad23 <osadchiydima23022006@gmail.com>
2023-12-16 04:10:23 +0300

    test sol

--- solution_test.py
+++ solution_test.py
@@ -1,6 +1,16 @@
 import pytest
 from solution import *
+from convex_hull import *
+from random import uniform
 
 s = Solution()
 
 def solution_test1():
+    for k in range(100):
+        coords = []
+        for i in range(3):
+            x = round(uniform(-10, 10), 1)
+            y = round(uniform(-10, 10), 1)
+            if not([x, y] in coords):
+                coords.append([x, y])
+        assert s.main(coords) == [[], 0]
